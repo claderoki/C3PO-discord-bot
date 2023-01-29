cdd8ec2c8784f4d7cef605274b2d195ca41ca397
Claderoki <49477117+claderoki@users.noreply.github.com>
2023-01-30 00:04:22 +0100

    no message

--- src/main/java/com/c3po/command/pigeon/PigeonProfileCommand.java
+++ src/main/java/com/c3po/command/pigeon/PigeonProfileCommand.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 @Component
 public class PigeonProfileCommand extends PigeonSubCommand {
+
     protected PigeonProfileCommand() {
         super("profile", "no description.");
         this.addOption(option -> option.name("user")
